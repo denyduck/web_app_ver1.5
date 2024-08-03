b0aef4d016b2172294b117ed0a371546e7c85356
danGit <deny@duck.com>
2024-08-04 00:55:11 +0200

    add req_dev.txt pro zavyslosti pro nasazeni vyvoj prosteedi aplikace

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
 
-    with app.app_context():
-        from . import routes
+#    with app.app_context():
+#        from . import routes
 
     return app
